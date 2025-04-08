ea766f1bfc8b172d6faebf70db47a9743935a0cc
harikrishnan.y <irah7708@gmail.com>
2025-04-08 23:31:49 +0530

    README File Updated

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -28,7 +28,7 @@
 
   <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
     <div class="container">
-      <a class="navbar-brand fw-bold text-blue-950" href="<?= route_to('products.index') ?>">Jwellary Product Management System</a>
+      <a class="navbar-brand fw-bold text-blue-950" href="<?= route_to('products.index') ?>">Jewellery Product Management System</a>
 
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
         <span class="navbar-toggler-icon"></span>
